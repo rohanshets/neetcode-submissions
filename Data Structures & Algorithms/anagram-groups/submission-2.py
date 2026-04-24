7704eb58045ed0663a6014d5fbb1a509d1244b1e
rohanshets <97878620+rohanshets@users.noreply.github.com>
2026-04-24 11:52:14 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        newLst = []
+        i = 0
+        j = 0
+        curr = strs.pop(0)
+        newLst.append([curr])
+        while strs != []:
+            if i >= len(strs):
+                i = 0
+                curr = strs.pop(0)
+                newLst.append([curr])
+                if strs == []:
+                    break
+                j += 1
+            if Counter(curr) == Counter(strs[i]):
+                newLst[j].append(strs.pop(i))
+            else:
+                i+=1
+        return newLst
+        
+            
